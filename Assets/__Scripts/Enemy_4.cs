5cf11e8b592fd57f1806691222efff35fa1f9ac5
dgil234 <dgil1026@gmail.com>
2019-03-26 16:47:02 -0400

    Find fixed. Shooting needs work

--- Assets/__Scripts/Enemy_4.cs
+++ Assets/__Scripts/Enemy_4.cs
@@ -71,8 +71,8 @@ public class Enemy_4 : Enemy {
             {
                 return (prt);
             }
-            return (null);
         }
+        return (null);
     }
     Part FindPart(GameObject go)
     {
@@ -82,81 +82,83 @@ public class Enemy_4 : Enemy {
             {
                 return (prt);
             }
-            return (null);
         }
+        return (null);
     }
     bool Destroyed(GameObject go)
-            {
-                return (Destroyed(FindPart(go)));
+    {
+        return (Destroyed(FindPart(go)));
     }
-    bool Destroyed (string n)
-            {
-                return (Destroyed(FindPart(n)));
+    bool Destroyed(string n)
+    {
+        return (Destroyed(FindPart(n)));
     }
     bool Destroyed(Part prt)
-            {
-                if (prt == null)
-                {
-                    return (true);
-                }
-                return (prt.health <= 0);
+    {
+        if (prt == null)
+        {
+            return (true);
+        }
+        return (prt.health <= 0);
     }
     void ShowLocalizedDamage(Material m)
-            {
-                m.color = Color.red;
-                damageDoneTime = Time.time + showDamageDuration;
-                showingDamage = true;
+    {
+        m.color = Color.red;
+        damageDoneTime = Time.time + showDamageDuration;
+        showingDamage = true;
     }
     void OnCollisionEnter(Collision coll)
-            {
-                GameObject other = coll.gameObject;
-                switch (other.tag)
+    {
+        GameObject other = coll.gameObject;
+        switch (other.tag)
+        {
+            case "ProjectileHero":
+                Projectile p = other.GetComponent<Projectile>();
+                if (!bndCheck.isOnScreen)
                 {
-                    case "ProjectileHero":
-                        Projectile p = other.GetComponent<Projectile>();
-                        if (!bndCheck.isOnScreen)
+                    Destroy(other);
+                    break;
+                }
+                GameObject goHit = coll.contacts[0].thisCollider.gameObject; // f
+                Part prtHit = FindPart(goHit);
+                if (prtHit == null)
+                { 
+                    goHit = coll.contacts[0].otherCollider.gameObject;
+                    prtHit = FindPart(goHit);
+                }
+                if (prtHit.protectedBy != null)
+                {
+                    foreach (string s in prtHit.protectedBy)
+                    {
+                        if (!Destroyed(s))
                         {
                             Destroy(other);
-                            break;
+                            return; 
                         }
-
-                        GameObject goHit = coll.contacts[0].thisCollider.gameObject;
-                        Part prtHit = FindPart(goHit);
-                        if (prtHit.protectedBy != null)
-                        {
-                            foreach(string s in prtHit.protectedBy)
-                            {
-                                if (!Destroyed(s))
-                                {
-                                    Destroy(other);
-                                    return;
-                                }
-                            }
-                        }
-
-                        prtHit.health -= Main.GetWeaponDefinition(p.type).damageOnHit;
-                        ShowLocalizedDamage(prtHit.mat);
-                        if (prtHit.health <= 0)
-                        {
-                            prtHit.go.SetActive(false);
-                        }
-                        bool allDestroyed=true;
-                        foreach(Part prt in parts)
-                        {
-                            if (!Destroyed(prt))
-                            {
-                                allDestroyed = false;
-                                break;
-                            }
-                        }
-
-                        if (allDestroyed)
-                        {
-                            Main.S.shipDestroyed(this);
-                            Destroy(this.gameObject);
-                        }
-                        Destroy(other);
-                        break;
+                    }
                 }
-    }
+                prtHit.health -= Main.GetWeaponDefinition(p.type).damageOnHit;
+                ShowLocalizedDamage(prtHit.mat);
+                if (prtHit.health <= 0)
+                {
+                    prtHit.go.SetActive(false);
+                }
+                bool allDestroyed = true;
+                foreach (Part prt in parts)
+                {
+                    if (!Destroyed(prt))
+                    {
+                        allDestroyed = false;
+                        break;
+                    }
+                }
+                if (allDestroyed)
+                {
+                    Main.S.shipDestroyed(this);
+                    Destroy(this.gameObject);
+                }
+                Destroy(other);
+                break;
+        }
+    }
 }
